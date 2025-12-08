efdf47768dbba59b0c25198122eee32d75b5a317
Abdurizo_Abdugafforov <abdontiy1998@mail.ru>
2025-12-08 22:23:28 +0500

    do

--- src/app/all-organization/all-organization.component.ts
+++ src/app/all-organization/all-organization.component.ts
@@ -1,36 +1,60 @@
 import { Component } from '@angular/core';
 
 interface Person {
-  key: string;
-  name: string;
-  age: number;
-  address: string;
+  organ: string;
+  status: boolean;
+  cours: number;
+  batch: number;
+  price: string;
+  actions: string;
 }
 
 @Component({
   selector: 'app-all-organization',
   templateUrl: './all-organization.component.html',
-  styleUrl: './all-organization.component.css'
+  styleUrl: './all-organization.component.css',
 })
 export class AllOrganizationComponent {
-listOfData: Person[] = [
+  listOfData: Person[] = [
     {
-      key: '1',
-      name: 'John Brown',
-      age: 32,
-      address: 'New York No. 1 Lake Park'
+      organ: 'Otcom',
+      status: true,
+      cours: 65,
+      batch: 1,
+      price: 'Male',
+      actions: '21.183.89.122',
     },
     {
-      key: '2',
-      name: 'Jim Green',
-      age: 42,
-      address: 'London No. 1 Lake Park'
+      organ: 'Sonair',
+      status: true,
+      cours: 59,
+      batch: 2,
+      price: 'Male',
+      actions: '1.77.48.233',
     },
     {
-      key: '3',
-      name: 'Joe Black',
-      age: 32,
-      address: 'Sidney No. 1 Lake Park'
-    }
+      organ: 'Bytecard',
+      status: true,
+      cours: 43,
+      batch: 3,
+      price: 'Male',
+      actions: '225.116.152.84',
+    },
+    {
+      organ: 'Overhold',
+      status: true,
+      cours: 72,
+      batch: 4,
+      price: 'Female',
+      actions: '81.106.120.39',
+    },
+    {
+      organ: 'Trippledex',
+      status: true,
+      cours: 69,
+      batch: 5,
+      price: 'Male',
+      actions: '37.195.86.174',
+    },
   ];
 }
